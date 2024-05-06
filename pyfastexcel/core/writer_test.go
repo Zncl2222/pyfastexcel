bc3b31f9b04a67ce96ef6c1d0e62285d0095fb3d
Zncl2222 <3002shinning@gmail.com>
2024-05-06 22:47:50 +0800

    test: add the test case with multiple sheets without 'Sheet1'

--- pyfastexcel/core/writer_test.go
+++ pyfastexcel/core/writer_test.go
@@ -2,34 +2,37 @@ package core
 
 import (
 	"encoding/base64"
+	"encoding/json"
+	"fmt"
 	"testing"
 )
 
-func TestWriteExcel(t *testing.T) {
-	// Mock input data
-	inputData := `{
-		"style": {
-			"style1": {
-				"Font": {
-					"Bold": true
+var data map[string]interface{}
+
+func init() {
+	data = map[string]interface{}{
+		"style": map[string]interface{}{
+			"style1": map[string]interface{}{
+				"Font": map[string]interface{}{
+					"Bold": true,
 				},
-				"Fill": {
+				"Fill": map[string]interface{}{
 					"Type":    "pattern",
 					"Color":   "#FFFFFF",
 					"Pattern": 1,
-					"Shading": 100
+					"Shading": 100,
 				},
-				"Border": {
-					"left": {
+				"Border": map[string]interface{}{
+					"left": map[string]interface{}{
 						"Color": "FF0000",
-						"Style": 1
+						"Style": 1,
 					},
-					"top": {
+					"top": map[string]interface{}{
 						"Color": "00FF00",
-						"Style": 2
-					}
+						"Style": 2,
+					},
 				},
-				"Alignment": {
+				"Alignment": map[string]interface{}{
 					"Horizontal":      "center",
 					"Vertical":        "middle",
 					"Indent":          0,
@@ -38,79 +41,89 @@ func TestWriteExcel(t *testing.T) {
 					"RelativeIndent":  0,
 					"ShrinkToFit":     false,
 					"TextRotation":    0,
-					"WrapText":        false
+					"WrapText":        false,
 				},
-				"Protection": {
+				"Protection": map[string]interface{}{
 					"Hidden": true,
-					"Locked": false
+					"Locked": false,
 				},
-				"CustomNumFmt": "0.00"
-			}
+				"CustomNumFmt": "0.00",
+			},
 		},
-		"file_props": {
-			"Title": "Test Excel File",
-			"Creator": "Test User",
-			"Category": "Test Category",
-			"ContentStatus": "Draft",
-			"Description": "Test Description",
-			"Keywords": "Test Keywords",
-			"Language": "en-US",
+		"file_props": map[string]interface{}{
+			"Title":          "Test Excel File",
+			"Creator":        "Test User",
+			"Category":       "Test Category",
+			"ContentStatus":  "Draft",
+			"Description":    "Test Description",
+			"Keywords":       "Test Keywords",
+			"Language":       "en-US",
 			"LastModifiedBy": "Test User",
-			"Revision": "1",
-			"Subject": "Test Subject",
-			"Version": "1.0",
-			"Identifier": "",
-			"Created": "",
-			"Modified": ""
+			"Revision":       "1",
+			"Subject":        "Test Subject",
+			"Version":        "1.0",
+			"Identifier":     "",
+			"Created":        "",
+			"Modified":       "",
 		},
-		"content": {
-			"Sheet1": {
-				"Header": [
-					["Column1", "Column2", "Column3"]
-				],
-				"Data": [
-					[["Data1", "style1"], ["Data2", "style1"], ["Data3", "style1"]],
-					[["Data4", "style1"], ["Data5", "style1"], ["Data6", "style1"]]
-				],
-				"Height": {"3": 252},
-				"Width": {"1": 25, "2": 26, "3": 6},
-				"MergeCells": []
+		"content": map[string]interface{}{
+			"TestingSheet2": map[string]interface{}{
+				"Header": [][]string{
+					{"Column1", "Column2", "Column3"},
+				},
+				"Data": [][][]string{
+					{{"Data1", "style1"}, {"Data2", "style1"}, {"Data3", "style1"}},
+					{{"Data4", "style1"}, {"Data5", "style1"}, {"Data6", "style1"}},
+				},
+				"Height":     map[string]int{"3": 252},
+				"Width":      map[string]int{"1": 25, "2": 26, "3": 6},
+				"MergeCells": []interface{}{},
 			},
-			"Sheet2WithNoWidth": {
-				"Header": [
-					["Column1", "Column2", "Column3"]
-				],
-				"Data": [
-					[["Data1", "style1"], ["Data2", "style1"], ["Data3", "style1"]],
-					[["Data4", "style1"], ["Data5", "style1"], ["Data6", "style1"]]
-				],
-				"Height": {"3": 252},
-				"MergeCells": [["A1", "A2"], ["B2","C3"]]
+			"Sheet2WithNoWidth": map[string]interface{}{
+				"Header": [][]string{
+					{"Column1", "Column2", "Column3"},
+				},
+				"Data": [][][]string{
+					{{"Data1", "style1"}, {"Data2", "style1"}, {"Data3", "style1"}},
+					{{"Data4", "style1"}, {"Data5", "style1"}, {"Data6", "style1"}},
+				},
+				"Height":     map[string]int{"3": 252},
+				"MergeCells": [][]interface{}{{"A1", "A2"}, {"B2", "C3"}},
 			},
-			"Sheet3WithNoHeight": {
-				"Header": [
-					["Column1", "Column2", "Column3"]
-				],
-				"Data": [
-					[["Data1", "style1"], ["Data2", "style1"], ["Data3", "style1"]],
-					[["Data4", "style1"], ["Data5", "style1"], ["Data6", "style1"]]
-				],
-				"Width": {"1": 25, "2": 26, "3": 6},
-				"MergeCells": []
+			"Sheet3WithNoHeight": map[string]interface{}{
+				"Header": [][]string{
+					{"Column1", "Column2", "Column3"},
+				},
+				"Data": [][][]string{
+					{{"Data1", "style1"}, {"Data2", "style1"}, {"Data3", "style1"}},
+					{{"Data4", "style1"}, {"Data5", "style1"}, {"Data6", "style1"}},
+				},
+				"Width":      map[string]int{"1": 25, "2": 26, "3": 6},
+				"MergeCells": []interface{}{},
 			},
-			"Sheet4WithNoWidthAndHeight": {
-				"Header": [
-					["Column1", "Column2", "Column3"]
-				],
-				"Data": [
-					[["Data1", "style1"], ["Data2", "style1"], ["Data3", "style1"]],
-					[["Data4", "style1"], ["Data5", "style1"], ["Data6", "style1"]]
-				],
-				"MergeCells": [["A1", "A2"], ["B2","C3"]]
-			}
-		}
-	}`
-	encodedExcel := WriteExcel(inputData)
+			"Sheet4WithNoWidthAndHeight": map[string]interface{}{
+				"Header": [][]string{
+					{"Column1", "Column2", "Column3"},
+				},
+				"Data": [][][]string{
+					{{"Data1", "style1"}, {"Data2", "style1"}, {"Data3", "style1"}},
+					{{"Data4", "style1"}, {"Data5", "style1"}, {"Data6", "style1"}},
+				},
+				"MergeCells": [][]interface{}{{"A1", "A2"}, {"B2", "C3"}},
+			},
+		},
+	}
+}
+
+func TestWriteExcel(t *testing.T) {
+	// Mock input data
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	encodedExcel := WriteExcel(string(jsonData))
 	decodedExcel, err := base64.StdEncoding.DecodeString(encodedExcel)
 	if err != nil {
 		t.Fatalf("Failed to decode encoded Excel data: %v", err)
@@ -120,4 +133,37 @@ func TestWriteExcel(t *testing.T) {
 	if len(decodedExcel) == 0 {
 		t.Error("Encoded Excel data is empty")
 	}
+}
+
+func TestWriteExcel2(t *testing.T) {
+	// Mock input data
+	data["content"] = map[string]interface{}{
+		"Sheet1": map[string]interface{}{
+			"Header": [][]string{
+				{"Column1", "Column2", "Column3"},
+			},
+			"Data": [][][]string{
+				{{"Data1", "style1"}, {"Data2", "style1"}, {"Data3", "style1"}},
+				{{"Data4", "style1"}, {"Data5", "style1"}, {"Data6", "style1"}},
+			},
+			"MergeCells": [][]interface{}{{"A1", "A2"}, {"B2", "C3"}},
+		},
+	}
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	encodedExcel := WriteExcel(string(jsonData))
+	decodedExcel, err := base64.StdEncoding.DecodeString(encodedExcel)
+	if err != nil {
+		t.Fatalf("Failed to decode encoded Excel data: %v", err)
+	}
+
+	// Assert the expected result
+	if len(decodedExcel) == 0 {
+		t.Error("Encoded Excel data is empty")
+	}
+
 }
