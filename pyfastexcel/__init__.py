b8d21efd913827ba23ac295156c104ee27d7ceed
Zncl2222 <3002shinning@gmail.com>
2024-05-17 21:52:09 +0800

    feat: make Customstyle and DefaultStyle able to import from pyfastexcel

--- pyfastexcel/__init__.py
+++ pyfastexcel/__init__.py
@@ -1,7 +1,13 @@
+from openpyxl_style_writer import CustomStyle, DefaultStyle
+
 from pyfastexcel.writer import FastWriter, NormalWriter, Workbook
 
 __all__ = [
     'Workbook',
     'FastWriter',
     'NormalWriter',
+    # Temporary link the CustomStyle from openpyxl_style_writer for
+    # convinent usage.
+    'CustomStyle',
+    'DefaultStyle',
 ]
