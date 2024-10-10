f081b5e6e8f87c6df620208ae2744a6e0927546d
Zncl2222 <3002shinning@gmail.com>
2024-10-10 20:20:11 +0800

    build: configure shared library setup for specific OS platforms

--- setup.py
+++ setup.py
@@ -1,3 +1,14 @@
 from setuptools import setup
+import platform
 
-setup()
+
+if platform.system() == 'Windows':
+    ext_files = ['*.dll']
+elif platform.system() == 'Linux':
+    ext_files = ['*.so']
+elif platform.system() == 'Darwin':
+    ext_files = ['*.dylib']
+else:
+    ext_files = []
+
+setup(include_package_data=True, package_data={'pyfastexcel': ext_files})
